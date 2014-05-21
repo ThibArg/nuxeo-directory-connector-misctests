488e1f659aea3d9686bd94bbbf53ae2733e30974
Thibaud Arguillere <thibarg.web@free.fr>
2014-05-21 15:55:37 -0400

    Add the NuxeoDemoComInMemoryConnector example

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
-nuxeo-directory-connector
-=========================
+nuxeo-directory-connector-misctests
+===================================
+
+## IMPORTANT
+
+This is fork of [nuxeo-directory-connector](https://github.com/tiry/nuxeo-directory-connector)
+
+Adding some examples and, hopefully, explanations.
+
+### The `NuxeoDemoComInMemoryConnector` DirectoryConnector
+
+This directory connects to demo.nuxeo.com and fetches documents which contains "nuxeo". It is a fulltext search.
+
+It extends `JsonInMemoryDirectoryConnector`, which means basically:
+* There is a _single_ call to the REST service
+* This call gets all documents which contain "nuxeo"
+  * The connector asks for max. 200 elements
+* It maps the result to the `entries` field of the JSON result. This field contains an array with all the found documents
+* Then, when the user enters something in the suggestion widget (bound to this directory), the internal query is done on the _pre-fetched_ data, so basically, we loop through the result (mapped to `entries`), and for each entry, we check if the `title` field contains the value.
+
+The class is commented, so you should find explanations in the code, but there is one other point which requests a bit more explanations: **We need to contribute an extension point, declaring this directory**, so nuxeo can use it. This is done in the `OSGI-INF/miscDirectoryConnectorsContrib.xml` file (which also contains comments):
+
+* A new directory is declared, named "demoNuxeoComDocuments"
+* This directory:
+  * Uses the `vocabulary` schema, which is declared in the `resources/schemas` folder
+  * Declares the class to be used to handle the vocabulary: Our `NuxeoDemoComInMemoryConnector` class
+  * Declares and maps the appropriate mapping between:
+    * The fields used in the `vocabulary` schema (`ìd` and `label`)
+    * And the JSON properties received after the call to the service (`uid` and `title`)
+  * Defines the URL to use: `http://demo.nuxeo.com/nuxeo/site/api/v1/path///@search?fullText=nuxeo`
+    * This URL will be called only once, the first time the user clicks in the Suggestion widget
+    * Then, when the user enters something in the widget, filtering is done on the existing, prefetched values: _No new request is sent to `demo.nuxeo.com`_.
+
+##### Summary: How to Use the `JsonInMemoryDirectoryConnector` (and its `BaseJSONDirectoryConnector` parent) with a `Vocabulary`-like table
+
+* Spend time checking the webservice you need to call, and look at the result it returns:
+  * Deduce the URL to call
+  * Deduce the mapping that must be done
+
+* Create a class wich extends `JsonInMemoryDirectoryConnector`, and override the following methods:
+  * `extractResult(JsonNode responseAsJson)`: You must here return the node which contains an array of your results
+  * `getEntryMap(String id)`: Because we are handling this example as a `vocabulary` schema, we must handle the `obsolete` field (see the example code)
+  * `call(String url)`: If you need to tune the call. In the `NuxeoDemoComInMemoryConnector`, we add the `Authorization` header and the `pageSize` query parameter
+  * `queryEntryIds(Map<String, Serializable> filter, Set<String> fulltext)`: This is the filter applied when the user enters values in the widget
+
+**NOTICE**: You dont' *need* tp override these methods, of course. If the default behavior feets you need, not problem :-)
+
+##### Using this Directory in a SuggestionWidget with Nuxeo Studio
+
+* Drag-drop a Directory Suggestion widget
+* Scroll the "Layout Widget Editor"
+* Click the "Custom properties configuration" to add a custom property
+* Enter "directoryName" as the key and the name of you directory as the value (in this example, "demoNuxeoComDocuments")
+* (save)
+
+That's all :-)
+
+(Like saying "that's all after spending quite sopme time building and explaining the thing :-) :-))
+
+==========
+==========
+Here is the original README
 
 ## What is this project ?
 
---
+++ src/main/java/org/nuxeo/directory/connector/json/nuxeodemo/NuxeoDemoComInMemoryConnector.java
@@ -0,0 +1,133 @@
+package org.nuxeo.directory.connector.json.nuxeodemo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.JsonNode;
+import org.nuxeo.directory.connector.json.JsonInMemoryDirectoryConnector;
+import org.nuxeo.ecm.core.api.ClientRuntimeException;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+/*
+ * In this example, we connect to a hard coded REST URL, defined in
+ * OSGI-INF/miscDirectoryConnectorsContrib.xml. This is why we are
+ * using the JsonInMemoryDirectoryConnector. BAsically what happens
+ * is:
+ *    - The query is done once, and gets all the results.
+ *      NOTE: In this example, we in,crease the default pageSize queryParameter
+ *      so we fetch more elements than the default (50). This is done in call(url).
+ *
+ *
+ *    - We override the call() method to handle authorization.
+ *
+ *    - We override extractResult() to get the "entries" array as returned
+ *      by the query
+ *
+ *    - We override queryEntryIds() to filter the list (when the user enters
+ *      something), so we filter on the correct field (in the JSON)
+ *
+ * Something IMPORTANT TO REMEMBER: This is the "...InMemoryConnector", so:
+ *      - The query is static
+ *      - The filter occurs only on the pre-fetched values, no new query is made
+ *        to the service
+ *      - It is "inMemory" => We  increase the "pageSize" parameter in the request,
+ *        and, obviously, the bigger the pageSize, the slower the initial request.
+ *              => You may want to take care of that in the initial request.
+ *              => As we request 200 (instead of 50) documents, we still maybe
+ *                 missing some, if the total was > 200 docs found.
+ *      - On the other hand, once the result is fetched, doing the sub-query is
+ *        pretty fast.
+ */
+public class NuxeoDemoComInMemoryConnector extends
+        JsonInMemoryDirectoryConnector {
+
+    protected Log log = LogFactory.getLog(NuxeoDemoComInMemoryConnector.class);
+
+    // Base64 for Administrator:Administrator
+    private static String kAUTHORIZATION = "Basic QWRtaW5pc3RyYXRvcjpBZG1pbmlzdHJhdG9y";
+    // Tune the pageSize
+    private static int kPAGE_SIZE = 50;
+
+    @Override
+    protected JsonNode call(String url) {
+
+        log.warn("COUCOUCOCUOCUOCUOU");
+
+        WebResource webResource = client.resource(url + "&pageSize=" + kPAGE_SIZE);
+        ClientResponse response = null;
+
+        response = webResource.accept("application/json")
+                              .header("Authorization", kAUTHORIZATION)
+                              .get(ClientResponse.class);
+
+        if (response.getStatus() != 200) {
+            throw new ClientRuntimeException(
+                    "Failed to call remote service : HTTP error code : "
+                            + response.getStatus());
+        }
+
+        try {
+            return getMapper().readTree(response.getEntityInputStream());
+        } catch (Exception e) {
+            throw new ClientRuntimeException(
+                    "Error while reading JSON response", e);
+        }
+
+    }
+
+    @Override
+    protected JsonNode extractResult(JsonNode responseAsJson) {
+        if( responseAsJson.get("numberOfPages").getIntValue() > 1) {
+            log.warn("Got only " + responseAsJson.get("currentPageSize") + " on " + responseAsJson.get("resultsCount") + " entities");
+        }
+        return responseAsJson.get("entries");
+    }
+
+    @Override
+    public Map<String, Object> getEntryMap(String id) {
+        Map<String, Object> entry = super.getEntryMap(id);
+        // add the obsolete flag so that the default directory filters will work
+        entry.put("obsolete", new Long(0));
+        return entry;
+    }
+
+    @Override
+    public List<String> queryEntryIds(Map<String, Serializable> filter,
+            Set<String> fulltext) {
+
+        List<String> ids = new ArrayList<String>();
+
+        String valueToFind = "";
+        if(filter.containsKey("label")) {
+            valueToFind = (String) filter.get("label");
+            if(valueToFind != null) {
+                valueToFind = valueToFind.toLowerCase();
+            }
+        }
+
+        // do the search
+        data_loop: for (String id : getEntryIds()) {
+
+            Map<String, Object> map = getEntryMap(id);
+            Object value = map.get("title");
+            if(value == null) {
+                continue data_loop;
+            }
+
+            if( value.toString().toLowerCase().indexOf(valueToFind) < 0) {
+                continue data_loop;
+            }
+
+            // this entry matches
+            ids.add(id);
+        }
+        return ids;
+    }
+}
